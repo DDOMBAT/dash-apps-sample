4ba8853de55f58cf531b3afa25009e7510d26240
DDOMBAT <dinsomniac67@gmail.com>
2024-10-29 15:16:21 -0400

    initial commit
    added python code

---
+++ helper_funcs.py
@@ -0,0 +1,9 @@
+
+URL = "https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv" 
+def get_url():
+    
+    return URL
+
+if __name__ == '__main__':
+    print("testing get URL")
+    print(get_url())
---
+++ minimal-app.py
@@ -0,0 +1,35 @@
+from dash import Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+from helper_funcs import get_url
+
+# reading the data into pandas 
+df = pd.read_csv(get_url())
+
+# instantiate dash app
+app = Dash()
+
+# layout of our app 
+app.layout = [
+    
+    # styled <h1> to be in the center and 
+    # have a background color of #84d8d8
+    html.H1(children='Population Growth by Year', style={'textAlign': 'center', 'background-color':'#84d8d8'}),
+    dcc.Dropdown(df.country.unique(), 'Armenia', id="dropdown-selection"), 
+    dcc.Graph(id="graphs-content")
+]
+
+# dataflow of the components 
+# register input/output
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_g(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
